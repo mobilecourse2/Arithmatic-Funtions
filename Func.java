fc5e3dfb76283107eb0338cdf32561d31fa82c52
Mobile Application Course <44363418+mobilecourse2@users.noreply.github.com>
2018-10-30 13:48:08 +0500

    Add files via upload

---
+++ Func.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Func
+{
+
+	public static void main(String[] args)
+	
+	{ 
+	
+	//find square
+	     int n, result;
+		 n = 10;
+		 result = square(n);
+		 System.out.println(result);
+		 
+		 n = 20;
+		 result = square(n);
+		 System.out.println(result);
+		 
+		 n = 30;
+		 result = square(n);
+		 System.out.println(result);
+		 
+		 n = 40;
+		 
+		 result = square(n);
+		 System.out.println(result);
+		 
+		 
+	}
+	
+	public static int square(int num)
+	{
+		return num*num; 
+		
+	}
+//addition of numbers using function
+ public static void add(int a, int b,int d)
+ {
+	 int c = a+b+d;
+	 System.out.println("Sum is "+c);
+ }
+ public static void add(int a, int b)
+ {
+	 int c = a+b;
+	 System.out.println("Sum is "+c);
+ }
+ public static void input()
+ {
+	     
+		 add(5,5,5);
+	 
+ }
+
+*/
+
+}
+
+
+
+
+ 
